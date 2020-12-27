81af9c8b9b2f670ff6161a06e26664d041ded7a1
Felipe Rosa Serpeloni <serpeloni@gmail.com>
2020-12-27 18:12:37 -0300

    - include field validation

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -18,6 +18,13 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
             "ativo",
         )
 
+    def validate_avaliacao(self, valor):
+        if valor in range(1, 6):
+            return valor
+        raise serializers.ValidationError(
+            "A avaliação precisa ser um inteiro entre 1 e 5"
+        )
+
 
 class CursoSerializer(serializers.ModelSerializer):
     # Nested Relationship
@@ -29,6 +36,7 @@ class CursoSerializer(serializers.ModelSerializer):
         many=True, read_only=True, view_name="avaliacao-detail"
     )
     """
+
     # Primary Key Related Field
     avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
