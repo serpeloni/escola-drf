d74825ea2506e98760fcbb434e0c588700fdf074
Felipe Rosa Serpeloni <serpeloni@gmail.com>
2020-12-27 18:05:06 -0300

    - configure throttle

--- escola-drf/settings.py
+++ escola-drf/settings.py
@@ -144,4 +144,12 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 2,
+    "DEFAULT_THROTTLE_CLASSES": (
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ),
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "5/minute",  # second, day, month, year
+        "user": "10/minute",
+    },
 }
